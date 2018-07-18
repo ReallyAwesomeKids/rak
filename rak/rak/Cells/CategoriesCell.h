c56b0cedc60f10051d4bfde69c1932be94b658ca
Halima Monds <hmonds@fb.com>
2018-07-17 17:47:26 -0700

    Images to categories

--- rak/rak/Cells/CategoriesCell.h
+++ rak/rak/Cells/CategoriesCell.h
@@ -9,8 +9,10 @@
 #import <UIKit/UIKit.h>
 #import "ActCategory.h"
 #import "Parse/Parse.h"
+#import "ParseUI/ParseUI.h"
+
 @interface CategoriesCell : UICollectionViewCell
-@property (weak, nonatomic) IBOutlet UILabel *categoriesView;
+@property (weak, nonatomic) IBOutlet PFImageView *categoriesImageView;
 @property (strong, nonatomic) ActCategory *cat;
 - (void)configureCell: (ActCategory *) cat;
 -(void)changeShape;
--- rak/rak/Cells/CategoriesCell.m
+++ rak/rak/Cells/CategoriesCell.m
@@ -14,9 +14,12 @@
 @implementation CategoriesCell
 - (void)configureCell: (ActCategory *) cat {
     self.cat = cat;
-    self.categoriesView.text = self.cat[@"categoryName"];
+    self.categoriesImageView.image = nil;
+    self.categoriesImageView.file = self.cat[@"categoryImage"];
+    [self.categoriesImageView loadInBackground];
 }
 -(void)changeShape{
+    self.categoriesImageView.layer.cornerRadius = self.categoriesImageView.frame.size.width/2;
     self.layer.cornerRadius = self.frame.size.width/2;
     self.layer.borderWidth = 2.0f;
     self.layer.borderColor = [UIColor blackColor].CGColor;
