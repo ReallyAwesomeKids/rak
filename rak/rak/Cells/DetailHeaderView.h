ec008000f1bcf3332bd7f81b3256eccef56860b6
Haley Zeng <hhzeng@fb.com>
2018-07-18 15:40:29 -0700

    finished autolayout for detail view

--- rak/rak/Cells/DetailHeaderView.h
+++ rak/rak/Cells/DetailHeaderView.h
@@ -1,16 +1,17 @@
 //
-//  DetailViewHeaderCell.h
+//  DetailHeaderView.h
 //  rak
 //
-//  Created by Haley Zeng on 7/17/18.
+//  Created by Haley Zeng on 7/18/18.
 //  Copyright © 2018 Really Awesome Kids. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 #import "Act.h"
 
-@interface DetailViewHeaderCell : UITableViewCell
+@interface DetailHeaderView : UIView
 
+@property (weak, nonatomic) IBOutlet UIStackView *stackView;
 @property (weak, nonatomic) IBOutlet UILabel *nameLabel;
 @property (weak, nonatomic) IBOutlet UILabel *pointsLabel;
 @property (weak, nonatomic) IBOutlet UILabel *completedCountLabel;
--- rak/rak/View Controllers/DetailViewController.m
+++ rak/rak/View Controllers/DetailViewController.m
@@ -8,12 +8,13 @@
 
 #import "DetailViewController.h"
 #import "CustomUser.h"
-#import "DetailViewHeaderCell.h"
+#import "DetailHeaderView.h"
 #import "DetailViewTableViewCell.h"
 
 
 @interface DetailViewController () <UITableViewDelegate, UITableViewDataSource>
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (weak, nonatomic) IBOutlet DetailHeaderView *headerView;
 @property (strong, nonatomic) NSArray *completionLog;
 @end
 
@@ -24,20 +25,30 @@
     self.tableView.delegate = self;
     self.tableView.dataSource = self;
     NSString *actObjectId = self.act.objectId;
-    self.completionLog = CustomUser.currentUser.actsDone[actObjectId];
+    NSArray *log = CustomUser.currentUser.actsDone[actObjectId];
+    self.completionLog = [[log reverseObjectEnumerator] allObjects];
     if (self.completionLog == nil)
         self.completionLog = [NSArray new];
+    self.headerView.act = self.act;
     [self.tableView reloadData];
+}
+
+- (void)viewDidLayoutSubviews {
+    [super viewDidLayoutSubviews];
+    DetailHeaderView *headerView = self.headerView;
+    [headerView setNeedsLayout];
+    [headerView layoutIfNeeded];
+    CGSize size = [headerView systemLayoutSizeFittingSize:UILayoutFittingCompressedSize];
+    CGFloat height = size.height;
+    CGRect frame = headerView.frame;
+    frame.size.height = height;
+    headerView.frame = frame;
+    
+    self.headerView = headerView;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     return self.completionLog.count;
-}
-
-- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section {
-    DetailViewHeaderCell *header = [tableView dequeueReusableCellWithIdentifier:@"DetailViewHeaderCell"];
-    header.act = self.act;
-    return header;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
