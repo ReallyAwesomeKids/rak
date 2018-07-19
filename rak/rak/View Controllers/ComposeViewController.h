6b8290ecc5642f7659ecd63e213a5f0ce5d00990
Gustavo Coutinho <guscoutinho@fb.com>
2018-07-19 15:57:48 -0700

    added compose view

---
+++ rak/rak/View Controllers/ComposeViewController.h
@@ -0,0 +1,13 @@
+//
+//  ComposeViewController.h
+//  rak
+//
+//  Created by Gustavo Coutinho on 7/19/18.
+//  Copyright © 2018 Really Awesome Kids. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ComposeViewController : UIViewController
+
+@end
