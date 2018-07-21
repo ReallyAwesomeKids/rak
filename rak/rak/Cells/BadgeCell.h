563dd6791b8f17905e82ed956a3df74136fdbee3
Haley Zeng <hhzeng@fb.com>
2018-07-20 17:08:37 -0700

    image for badge cell

--- rak/rak/Cells/BadgeCell.h
+++ rak/rak/Cells/BadgeCell.h
@@ -8,12 +8,13 @@
 
 #import <UIKit/UIKit.h>
 #import "Badge.h"
+#import <ParseUI/ParseUI.h>
 
 @interface BadgeCell : UICollectionViewCell
 
 @property (strong, nonatomic) Badge *badge;
 
-@property (weak, nonatomic) IBOutlet UIImageView *badgeImageView;
+@property (weak, nonatomic) IBOutlet PFImageView *badgeImageView;
 
 @property (weak, nonatomic) IBOutlet UILabel *badgeNameLabel;
 
--- rak/rak/Cells/BadgeCell.m
+++ rak/rak/Cells/BadgeCell.m
@@ -13,6 +13,9 @@
 - (void)setBadge:(Badge *)badge {
     _badge = badge;
     self.badgeNameLabel.text = badge.badgeName;
+    self.badgeImageView.file = badge.badgeImage;
+    [self.badgeImageView loadInBackground];
+    
 }
 
 @end
