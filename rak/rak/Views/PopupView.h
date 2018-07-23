c77cef168dd2b7259c5c553f41e6e8d21f9448a4
Haley Zeng <hhzeng@fb.com>
2018-07-23 12:21:46 -0700

    workign on popup view

--- rak/rak/Views/PopupView.h
+++ rak/rak/Views/PopupView.h
@@ -9,4 +9,5 @@
 
 @interface PopupView : UIView
 
+
 @end
--- rak/rak/Views/PopupView.m
+++ rak/rak/Views/PopupView.m
@@ -7,14 +7,39 @@
 
 #import "PopupView.h"
 
+@interface PopupView ()
+
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *achievementImageView;
+@property (weak, nonatomic) IBOutlet UILabel *descriptionLabel;
+@property (weak, nonatomic) IBOutlet UIButton *shareButton;
+@property (weak, nonatomic) IBOutlet UIButton *closeButton;
+
+@end
+
 @implementation PopupView
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+- (instancetype)initWithCoder:(NSCoder *)coder
+{
+    self = [super initWithCoder:coder];
+    if (self) {
+        [self customInit];
+    }
+    return self;
 }
-*/
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        [self customInit];
+    }
+    return self;
+}
+
+- (instancetype)customInit {
+    [[NSBundle mainBundle] loadNibNamed:@"PopupViewXIB" owner:self options:nil];
+    return self;
+}
 
 @end
