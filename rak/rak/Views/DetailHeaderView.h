f24d00bee1b6d998ee7bc7ba9acf43323619a298
Gustavo Coutinho <guscoutinho@fb.com>
2018-07-25 10:31:06 -0700

    cleaned code in Views, except PopupView (under edition currently)

--- rak/rak/Views/DetailHeaderView.h
+++ rak/rak/Views/DetailHeaderView.h
@@ -1,11 +1,3 @@
-//
-//  DetailHeaderView.h
-//  rak
-//
-//  Created by Haley Zeng on 7/18/18.
-//  Copyright © 2018 Really Awesome Kids. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 #import "Act.h"
 
--- rak/rak/Views/ProfileHeader.h
+++ rak/rak/Views/ProfileHeader.h
@@ -1,11 +1,3 @@
-//
-//  ProfileHeader.h
-//  rak
-//
-//  Created by Haley Zeng on 7/16/18.
-//  Copyright © 2018 Really Awesome Kids. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 #import <ParseUI/ParseUI.h>
 
@@ -14,7 +6,6 @@
 @interface ProfileHeader : UICollectionViewCell	
 
 @property (strong, nonatomic) CustomUser *user;
-
 @property (weak, nonatomic) IBOutlet PFImageView *profileImageView;
 @property (weak, nonatomic) IBOutlet UILabel *displayNameLabel;
 @property (weak, nonatomic) IBOutlet UILabel *locationLabel;
