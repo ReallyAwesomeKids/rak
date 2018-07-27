a6cd1f9dc4bfe968eff66a6a2dcb4aca62127709
Haley Zeng <hhzeng@fb.com>
2018-07-27 11:36:05 -0700

    pins stored in db

---
+++ rak/rak/Models/Parse Objects/MapPin.h
@@ -0,0 +1,24 @@
+//
+//  MapPin.h
+//  rak
+//
+//  Created by Haley Zeng on 7/27/18.
+//  Copyright © 2018 Really Awesome Kids. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <Parse/Parse.h>
+
+@interface MapPin : PFObject <PFSubclassing>
+
+@property (strong, nonatomic) NSNumber *latitude;
+@property (strong, nonatomic) NSNumber *longitude;
+@property (strong, nonatomic) NSString *locationName;
+@property (strong, nonatomic) NSString *locationDescription;
+
+- (instancetype)initWithLatitude:(NSNumber *)latitude
+                       longitude:(NSNumber *)longitude
+                            name:(NSString *)locationName
+                     description:(NSString *)locationDescription;
+
+@end
