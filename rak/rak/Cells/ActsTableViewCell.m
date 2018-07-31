6b4149fc761883642da743c8d1b755c3a0ba4439
Gustavo Coutinho <guscoutinho@fb.com>
2018-07-30 17:06:55 -0700

    when pressing checkmark in cell button, it animates to greenish and then back to gray

--- rak/rak/Cells/ActsTableViewCell.m
+++ rak/rak/Cells/ActsTableViewCell.m
@@ -4,7 +4,6 @@
 
 - (void)awakeFromNib {
     [super awakeFromNib];
-    // Initialization code
 }
 
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated {
@@ -14,6 +13,20 @@
 - (void)setAct:(Act *)act {
     _act = act;
     self.homeCellActName.text = self.act.actName;
+}
+
+- (IBAction)didTapCellCheckmark:(id)sender {
+    [self.checkButton setAlpha:0.f];
+    [self.checkButton setImage:[UIImage imageNamed:@"check-filled"] forState:UIControlStateNormal];
+    
+    // Animates the button from alpha 0 and green back to alpha 1 and gray
+    [UIView animateWithDuration:2.f delay:0.f options:UIViewAnimationOptionCurveEaseIn animations:^{
+        [self.checkButton setAlpha:1.f];
+    } completion:^(BOOL finished) {
+        [UIView animateWithDuration:2.f delay:0.f options:UIViewAnimationOptionCurveEaseInOut animations:^{
+                [self.checkButton setImage:[UIImage imageNamed:@"check"] forState:UIControlStateNormal];
+        } completion:nil];
+    }];
 }
 
 @end
